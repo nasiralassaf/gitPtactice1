1ff7b1fac7aadfddf6a3bd54d5b675450b829ee8
nasiralassaf <nasiralassaf@outlook.com>
2020-04-27 22:41:08 -0400

    nn

--- src/Test/PPP.java
+++ src/Test/PPP.java
@@ -1,4 +1,21 @@
 package Test;
 
+
+import java.util.Scanner;
+
 public class PPP {
-}
+    public static void main(String[] args) {
+        /*
+
+         */
+
+        Scanner s = new Scanner(System.in);
+
+        String n = s.next();
+
+        int count = 0;
+        for (int i = 0; i < 0; i++){
+
+        }
+         }
+   }
